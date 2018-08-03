69e5a1ee6bf7c0e063c37a582d37210320ab6eda
Naveen <naveen@anilabsinc.com>
2018-08-03 15:53:46 +0530

    print key value

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,12 +1,12 @@
 rails_env = "production"
   
-#  Chef::Log.info("Mapping the environment_variables node for RAILS_ENV=#{rails_env}...")
-#  node[:deploy].each do |application, deploy|
-#    deploy[:environment_variables].each do |key, value|
-#      ENV[key] = value
-#    end
-#  end
-  
+ Chef::Log.info("Mapping the environment_variables node for RAILS_ENV=#{rails_env}...")
+ node[:deploy].each do |application, deploy|
+   deploy[:environment_variables].each do |key, value|
+   Chef::Log.info("Precompiling assets for #{key}...#{value} ")
+     ENV[key] = value
+   end
+ end
  Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
  execute "rake assets:precompile" do
    cwd release_path
